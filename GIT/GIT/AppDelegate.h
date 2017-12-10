eb779c22f5699c01e1f8f4e85f67cc5493900159
958386952@qq.com <958386952@qq.com>
2017-12-10 15:07:25 +0800

    create

---
+++ GIT/GIT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIT
+//
+//  Created by 胡建波 on 2017/12/10.
+//  Copyright © 2017年 Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GIT/GIT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GIT
+//
+//  Created by 胡建波 on 2017/12/10.
+//  Copyright © 2017年 Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
